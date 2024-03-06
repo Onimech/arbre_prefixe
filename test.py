fbef8f9f72e09e5377d345174f3db35eeb9e2065
Onimech <noah.chemin17430@gmail.com>
2024-03-06 16:54:52 +0100

    liste des suffixe

--- test.py
+++ test.py
@@ -4,24 +4,42 @@ def main():
     k = int(input("Entrez la longueur k : "))
     sequence = input("Entrez la séquence voulue :")
     alphabet = caracteres_diff(sequence)
-    liste_suffixe=[""]
+    liste_suffixe = [""]
     arbre = init_arbre(alphabet)
+    remplissage_abr(sequence, arbre, alphabet, liste_suffixe, k)
     print(arbre)
+
 
 def caracteres_diff(seq):
     return set(seq)
-
 
 def init_arbre(alphabet):
     A = {}
     A["clé"] = 0
     for l in alphabet:
-        A[l]={}
+        A[l] = {}
     return A
 
-def remplisage_abr(seq, ):
-    for i in 1..len(seq):
-        a=1
+def remplissage_abr(seq, arbre, alphabet, LS, k):
+    for caractere in seq:
+        if caractere in alphabet:
+            LS[:] = [mot + caractere for mot in LS]
+            LS.append("")
+            for suffixe in LS:
+                if len(suffixe)>k:
+                    LS.remove(suffixe)
+        
+main()
+
+
+
+            # if arbre[caractere] == {}:
+            #     arbre[caractere] = {'clé': 1, 'T': {}, 'C': {}, 'G': {}, 'A': {}}  # Initialisez le compteur à 1
+            # else:
+            #     arbre[caractere]['clé'] += 1  # Incrémentation du compteur
+
+
+
 
 
 
