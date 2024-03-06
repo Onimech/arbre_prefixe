d7bed68b3b6c68729c3a90526306494af31a3c19
Onimech <noah.chemin17430@gmail.com>
2024-03-06 14:27:29 +0100

    commentaires

--- test.py
+++ test.py
@@ -13,6 +13,13 @@ def main ():
 
 def caracteres_diff(seq):
      return(set(seq))
+#for i in nbalphabet
+#new fils"i"
 
+#noeud des arbre : liste contenant le dernier caractere du motif et les positions du motif
+
+
+#registe des derniers motifs rencontrés, ajout du caractère rencontré aux motifs
+#elimination des motifs de longueur supérieure à k
 
 main()
