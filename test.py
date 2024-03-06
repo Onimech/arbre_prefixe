2c66a313c8603de5dc176ef7ac01cdda7b7cf90b
Onimech <noah.chemin17430@gmail.com>
2024-03-06 14:11:41 +0100

    ah c'est bon la

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ def creaAbr(m, k):
 def main ():
      motif = input("Entrez la motif : ")
      k = input("Entrez la longueur k : ")
+     sequence = input("Entrez la séquence voulue :")
+     alphabet = caracteres_diff(sequence)
+     print(sequence)
+     print(alphabet)
+
+def caracteres_diff(seq):
+     return(set(seq))
+
+
+main()
